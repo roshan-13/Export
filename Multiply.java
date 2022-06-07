b26b000bfcbf7cf189675d86c488ecd32b1961bd
Roshan <roshanpatil111@gmail.com>
2022-06-07 12:22:17 +0530

    Multiply.java added

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		int c = ( a * b );
+        System.out.println("Multtiplication is ===> "+c); 
+    }
+}
